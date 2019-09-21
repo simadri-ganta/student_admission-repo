2fc04ea96453d03a45032b0d0c04497a469a75ff
simadri-ganta <46081258+simadri-ganta@users.noreply.github.com>
2019-09-21 15:20:44 +0530

    modified code

--- demo/src/main/java/com/jntu/sas/repositories/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jntu.sas.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jntu.sas.beans.Admin;
-
-public interface AdminRepo extends JpaRepository<Admin, String> {
-
-}
